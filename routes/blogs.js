00af97e18f4bf9f2dd242d90eb4fa28ff979badb
Harry Nguyen <hieu.fs8@gmail.com>
2022-09-17 19:36:10 +0300

    blogs: get, put method

--- routes/blogs.js
+++ routes/blogs.js
@@ -9,4 +9,34 @@ blogsRouter.get('/', async (_req, res) => {
   res.json(blogs)
 })
 
+blogsRouter.get('/:id', async (req, res, next) => {
+  try {
+    const blog = await Blog.findById(req.params.id)
+    if (blog) {
+      res.json(blog)
+    } else {
+      res.status(404).end()
+    }
+  }
+  catch(error) {
+    next(error)
+  }
+})
+
+blogsRouter.put('/:id', (req, res, next) => {
+  const { title, author, url, likes } = req.body
+
+  Blog
+    .findByIdAndUpdate(
+      req.params.id,
+      { title, author, url, likes },
+      { new: true, runValidators: true, context: 'query' }
+    )
+    .then(updatedBlog => {
+      if (updatedBlog) { res.status(200).json(updatedBlog) }
+      else { res.status(404).end() }
+    })
+    .catch(error => next(error))
+})
+
 module.exports = blogsRouter
