479f2792b4b0ae6870c4568a0e4c6c71a0e704e4
Harry Nguyen <hieu.fs8@gmail.com>
2022-09-17 17:48:19 +0300

    basic middleware

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 app.get('/', (req, res) => {
@@ -18,7 +20,15 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('------ error connection to MongoDB:', error.message)
   })
 
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
 const blogsRouter = require('./routes/blogs')
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
