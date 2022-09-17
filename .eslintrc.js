eb108c4876af2c01a7af8da90e3ad24a1e6beecc
Harry Nguyen <hieu.fs8@gmail.com>
2022-09-17 13:41:19 +0300

    init express app

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'node': true,
+    'commonjs': true,
+    'es2021': true,
+    'jest': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2 // NOTE:
+    ],
+    // 'linebreak-style': [
+    //   'error',
+    //   // 'unix' // LF
+    //   'windows' // CRLF
+    // ],
+    'linebreak-style': 0, // NOTE: avoid CRLF
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'  //  NOTE: semicolon - option ALWAYS 
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error', // NOTE:
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [ // NOTE:
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 0, // NOTE:
+  },
+}
