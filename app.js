ef7dca9739acfdb28c0f15a8adb7e4998cf6e3aa
Harry Nguyen <hieu.fs8@gmail.com>
2024-08-04 23:47:19 +0300

    Fix: correct dir for static file, also update this static file

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 app.use(cors())
-app.use(express.static('build'))
+app.use(express.static(path.resolve(__dirname, 'build')))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
@@ -38,7 +38,7 @@ app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 
 app.get('*', function (_req, res) {
-  res.sendFile(path.resolve('build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 })
 // app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
